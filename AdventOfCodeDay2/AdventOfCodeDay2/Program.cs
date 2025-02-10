46d9af9faaa07c724874becaf2a17c9668f57aba
Thesupernile <62834678+Supernile@users.noreply.github.com>
2025-02-10 20:59:38 +0000

    Add files via upload

---
+++ AdventOfCodeDay2/AdventOfCodeDay2/Program.cs
@@ -0,0 +1,150 @@
+﻿class Program()
+{
+    static bool IsSafe(int[] report, bool firstCall)
+    {
+        bool ascending = false;
+        bool reportSafe = true;
+        bool problemDampenerUsed = true;
+        if (firstCall)
+        {
+            problemDampenerUsed = false;
+        }
+        int removedIndex = -1;
+        // Check to see if the sequence is ascending or descending
+        if (report[1] > report[0])
+        {
+            ascending = true;
+        }
+        else if (report[1] < report[0])
+        {
+            ascending = false;
+        }
+        else
+        {
+            reportSafe = false;
+        }
+
+        //A report is safe if each number in the list is either descending or ascending (by less than 3 at a time)
+        for (int i = 1; i < report.Length; i++)
+        {
+            if (ascending)
+            {
+                //Checks first whether the index is less than the last (if it is then the report is not safe) then if the index is three or more apart (if it is then report not safe)
+                if (report[i] <= report[i - 1])
+                {
+                    reportSafe = false;
+                    if (problemDampenerUsed == false)
+                    {
+                        removedIndex = i;
+                        problemDampenerUsed = true;
+                    }
+                }
+                //Checks first whether the index is greater than the last (if it is then the report is not safe) then if the index is three or more apart (if it is then report not safe)
+                else if (((report[i] - report[i - 1]) > 3) || ((report[i] - report[i - 1]) < 1))
+                {
+                    reportSafe = false;
+                    if (problemDampenerUsed == false)
+                    {
+                        removedIndex = i;
+                        problemDampenerUsed = true;
+                    }
+                }
+            }
+            else
+            {
+                //Checks first whether the index is greater than the last (if it is then the report is not safe) then if the index is three or more apart (if it is then report not safe)
+                if (report[i] >= report[i - 1])
+                {
+                    reportSafe = false;
+                    if (problemDampenerUsed == false)
+                    {
+                        removedIndex = i;
+                        problemDampenerUsed = true;
+                    }
+                }
+                else if (((report[i - 1] - report[i]) > 3) || ((report[i - 1] - report[i]) < 1))
+                {
+                    reportSafe = false;
+                    if (problemDampenerUsed == false)
+                    {
+                        removedIndex = i;
+                        problemDampenerUsed = true;
+                    }
+                }
+
+                if (i <= report.Length - 1)
+                {
+                    if (report[i] == report[i - 1])
+                    {
+                        reportSafe = false;
+                    }
+                }
+
+            }
+        }
+        
+        if (!reportSafe && firstCall == true)
+        {
+            int i = 0;
+            while (reportSafe == false && i < report.Length)
+            {
+                int[] newReport = CreateNewReport(report, i);
+                reportSafe = IsSafe(newReport, false);
+                i++;
+            }
+        }
+        
+        return reportSafe;
+    }
+
+    static int[] CreateNewReport(int[] report, int removedIndex)
+    {
+        int[] newReport = new int[(report.Length - 1)];
+        int j = 0;
+        for (int i = 0; i < report.Length; i++)
+        {
+            if (i != removedIndex)
+            {
+                newReport[j] = report[i];
+                j++;
+            }
+        }
+        return newReport;
+
+    }
+    static int CheckSafeReports()
+    {
+        const string FILEPATH = "test.txt";
+        int numberOfSafeReports = 0;
+        List<string> stringReport = new List<string>();
+        bool reportSafe = false;
+
+        using (StreamReader sr = new StreamReader(FILEPATH))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                stringReport.AddRange(line.Split(" "));
+                int[] report = new int[stringReport.Count];
+                for (int i = 0; i < stringReport.Count; i++)
+                {
+                    report[i] = Convert.ToInt32(stringReport[i]);
+                }
+                reportSafe = IsSafe(report, true);
+                if (reportSafe)
+                {
+                    numberOfSafeReports++;
+                }
+                stringReport = new List<string>();
+            }
+        }
+
+        return numberOfSafeReports;
+    }
+
+    static void Main()
+    {
+        int numberOfSafeReports = CheckSafeReports();
+        Console.WriteLine($"There are {numberOfSafeReports} safe reports");
+    }
+}
---
+++ AdventOfCodeDay3/AdventOfCodeDay3/Program.cs
@@ -0,0 +1,119 @@
+﻿class Program
+{
+    static string ExtractCommand(string inputString, int startingIndex)
+    {
+        //extracts the command from the string
+        int num = 0;
+        bool commaReached = false;
+        string command = string.Empty;
+        int numberLength = 0;
+
+        if (inputString.Substring(startingIndex , 4) == "mul(")
+        {
+            //command += "mul(";
+            for (int i = startingIndex + 4; i < startingIndex + 12; i++)
+            {
+                if (int.TryParse("" + inputString[i], out num) && numberLength < 3)
+                {
+                    command += inputString[i];
+                    numberLength += 1;
+                }
+                else if (inputString[i] == ',' && numberLength >= 1 && commaReached != true)
+                {
+                    command += inputString[i];
+                    numberLength = 0;
+                    commaReached = true;
+                }
+                else if (inputString[i] == ')' && numberLength >= 1 && commaReached == true)
+                {
+                    //command += inputString[i];
+                    //Makes i out of range to end the for loop
+                    i += 8;
+                }
+                else
+                {
+                    command = string.Empty;
+                    //Makes i out of range to end the for loop
+                    i += 8;
+                }
+            }
+        }
+
+        return command;
+    }
+
+
+    static List<string> RemoveInvalidCommands(string inputString)
+    {
+        //A valid command must be in the format mul(x,y) where x and y are numbers with up to three digits
+        int startingIndex = 0;
+        int endIndex = 0;
+        bool doCommands = true;
+        string command = string.Empty;
+        List<string> commands = new List<string>();
+
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            if (inputString[i] == 'm')
+            {
+                startingIndex = i;
+                if (doCommands == true)
+                {
+                    command = ExtractCommand(inputString, startingIndex);
+                    if (command != "")
+                    {
+                        commands.Add(command);
+                    }
+                }
+            }
+            else if (inputString[i] == 'd')
+            {
+                startingIndex = i;
+                if (inputString.Substring(startingIndex, 7) == "don't()")
+                {
+                    doCommands = false;
+                }
+                else if (inputString.Substring(startingIndex, 4) == "do()")
+                {
+                    doCommands = true;
+                }
+            }
+        }
+
+        return commands;
+    }
+
+    static int PerformCommands(List<string> commands)
+    {
+        string command = string.Empty;
+        string[] nums = new string[2];
+        int num1 = 0;
+        int num2 = 0;
+        int result = 0;
+        int totalResult = 0;
+
+        for (int i = 0; i <= commands.Count - 1; i++)
+        {
+            command = commands[i];
+            nums = command.Split(',');
+            num1 = Convert.ToInt32(nums[0]);
+            num2 = Convert.ToInt32(nums[1]);
+            result = num1 * num2;
+            totalResult += result;
+        }
+
+        return totalResult;
+    }
+
+
+    static void Main()
+    {
+        const string FILEPATH = "test.txt";
+
+        string inputString = File.ReadAllText(FILEPATH);
+        List<string> commands = RemoveInvalidCommands(inputString);
+        int result = PerformCommands(commands);
+        
+        Console.WriteLine(result);
+    }
+}
